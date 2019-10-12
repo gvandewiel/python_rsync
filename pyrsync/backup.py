1883b86c3ec79b111b68f895db9f1f5b47115519
Gijs van de Wiel <gvandewiel@gmail.com>
2019-10-12 20:30:14 +0200

    Working on RegEx to find total number of files

--- pyrsync/backup.py
+++ pyrsync/backup.py
@@ -424,7 +424,7 @@ class Backup():
         self.logger.info('{}'.format(out))
 
         self.logger.info('Checking output')
-        mn = re.findall(r'Number of files: (\d+,\d+)', str(out))
+        mn = re.findall(r'Number of files: (\d+)', out)
         total_files = int(mn[0].replace(',',''))
         print('Number of files: ' + str(total_files))
 
