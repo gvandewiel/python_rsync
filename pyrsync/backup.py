009f66c4ad145a0a21a3eab4a6e385e96ec7a391
Gijs van de Wiel <gvandewiel@gmail.com>
2019-10-13 12:57:03 +0200

    Updated typos in logger output

--- pyrsync/backup.py
+++ pyrsync/backup.py
@@ -242,12 +242,12 @@ class Backup():
             if self.__RemoteServerCheck__(job):
                 # Start rsync backup
                 self.rsync(job)
-                self.logger.info(c.OKGREEN + c.BOLD + '  * Finished rsync job' + c.END)
+                self.logger.info(c.OKGREEN + c.BOLD + '  * Finished rsync job' + c.ENDC)
                 # Update current directory
                 if job.dry_run:
                     self.logger.info(c.WARNING + c.BOLD + '  * "--dry-run" detected, no update of statefile.' + c.ENDC)
                 else:
-                    self.logger.info(c.OKGREEN + c.BOLD + '  * Updating statefile' + c.END)
+                    self.logger.info(c.OKGREEN + c.BOLD + '  * Updating statefile' + c.ENDC)
                     job.update_state()
                     self.logger.info(c.OKBLUE + c.BOLD + '  * Updated statefile with hash "{}" to {}'.format(job.hash, job.new_id) + c.ENDC)
                 
@@ -450,9 +450,6 @@ class Backup():
             self.logger.info(c.FAIL + c.BOLD + '  * Rsync exited with errorcode {}'.format(p.returncode) + c.ENDC)
             raise sp.CalledProcessError(p.returncode, p.args)
             job.log_file.close()
-        else:
-            print('passing....')
-            pass
 
 
 if __name__ == '__main__':
